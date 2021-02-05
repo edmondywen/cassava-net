d756e2fc3944c1888d58cc2d12f271099dec0e40
edmondywen <edmondywen@gmail.com>
2021-02-04 19:52:19 -0800

    First version of CNNDataset class is done

--- datasets/CNNDataset.py
+++ datasets/CNNDataset.py
@@ -7,38 +7,23 @@ class CNNDataset(torch.utils.data.Dataset):
     Dataset that contains 100000 3x224x224 black images (all zeros).
     """
 
-    #800 x 600 - you want to reduce the image resolution using pytorch (transform) or pillow
-    #need some way to load image into a tensor 
-    #imagePath should be a string containing file path to where the data is. use pillow to get a numpy array image.open
+    # 800 x 600 - you want to reduce the image resolution using pytorch (transform) or pillow
+    # need some way to load image into a tensor 
+    # imagePath should be a string containing file path to where the data is. 
+    # use pillow to get a numpy array image.open
+
     def __init__(self, inputs, labels, imagePath): #try passing in csv
         df = pd.read_csv(imagePath) 
-        self.inputImages = df['image_id'] #get the entire 
-        self.truthLabels = labels 
-        self.imageSource = imagePath
-
+        self.inputImages = df['image_id'] #get the entire first column
+        self.truthLabels = labels['label'] #get the second column
 
     def __getitem__(self, index): #allows you to access with square brackets
         #inputs = torch.zeros([3, 224, 224])
-        inputs = self.inputData[index]
-        labels = self.truthLabels[index]
-        return (inputs, labels)
+        #cache images potentially? 
+        input = Image.open(self.inputImages[index])
+        input = input.resize((224, 224)) #use center crop after if you want the whole image 
+        label = self.truthLabels[index]
+        return (input, label)
 
     def __len__(self): #redefines length
-        return len(self.inputData)
-
-class StartingDataset(torch.utils.data.Dataset):
-    """
-    Dataset that contains 100000 3x224x224 black images (all zeros).
-    """
-
-    def __init__(self):
-        pass
-
-    def __getitem__(self, index):
-        inputs = torch.zeros([3, 224, 224])
-        label = 0
-
-        return inputs, label
-
-    def __len__(self):
-        return 10000
+        return len(self.inputImages)
