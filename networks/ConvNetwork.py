f6f14c3122593b4abace10733ddaecb4c04cf5ba
edmondywen <edmondywen@gmail.com>
2021-02-11 19:32:06 -0800

    updating convnetwork.py

--- networks/ConvNetwork.py
+++ networks/ConvNetwork.py
@@ -4,18 +4,38 @@ import torch.nn.functional as F
 
 class ConvNetwork(torch.nn.Module):
     def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 4, 4) #4,4 convolution on a 3-channeled image
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(220 * 220 * 3, 1)
-        self.sigmoid = nn.Sigmoid()
+        self.fc1 = torch.linear()
+        self.fc2 = torch.linear()
+        self.relu = torch.relu
+        self.conv1 = torch.Conv2d(3, ) #technically a member variable. you can call objects like functions tho. 
+        self.conv2 = torch.Conv2d()
+        self.pool = torch.maxpool()
+
+        # self.resnet = torch.hub.load('pytorch/vision', 'resnet18', pretrained=True)
+        # self.resnet = self.resnet[:-1] #cut off last layer. fix syntax
+        # self.resnet.eval()
     
     def forward(self, x):
         x = self.conv1(x)
-        x = self.flatten(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        x = self.pool(x)
         x = self.fc1(x)
-        x = self.sigmoid(x) 
-        return x
+        x = self.pool(x)
+        x = self.fc2(x)
+
+        # transfer learning - comment out above to implement
+        # with torch.no_grad():
+        #     x = self.resnet(x)
+
+        # #do reshaping to make sure output of resnet will fit in 
+
+        # #append fully connected layers
+        # x = self.fc1(x)
+        # x = self.relu(x)
+        # x = self.fc2(x)
+
+        return(x)
 
 class StartingNetwork(torch.nn.Module):
     """
@@ -28,9 +48,7 @@ class StartingNetwork(torch.nn.Module):
         # self.fc1 = nn.Linear(224 * 224 * 3, 1)
         # self.fc2 = nn.Linear(100, 1)
         # self.sigmoid = nn.Sigmoid()
-        self.resnet = torch.hub.load('pytorch/vision', 'resnet18', pretrained=True)
-        self.resnet = self.resnet[:-1] #cut off last layer. fix syntax
-        self.resnet.eval()
+
 
     def forward(self, x):
         # x = self.flatten(x)
@@ -38,16 +56,6 @@ class StartingNetwork(torch.nn.Module):
         # x = F.relu(x)
         # x = self.fx2(x)
         # x = self.sigmoid(x)
-
-        with torch.no_grad():
-            x = self.resnet(x)
-
-        #do reshaping to make sure output of resnet will fit in 
-
-        #append fully connected layers
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
 
         return x
 
---
+++ networks/StartingNetwork.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class StartingNetwork(torch.nn.Module):
+    """
+    Basic logistic regression on 224x224x3 images.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(224 * 224 * 3, 1)
+        self.fc2 = nn.Linear(100, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fx2(x)
+        x = self.sigmoid(x)
+
+        with torch.no_grad():
+            x = self.resnet(x)
+
+        #do reshaping to make sure output of resnet will fit in 
+
+        #append fully connected layers
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+
+        return x
